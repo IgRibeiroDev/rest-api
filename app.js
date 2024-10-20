4c180d3def4210e53bf5a9109c494fcda0e31e86
IgRibeiroDev <igribeiro.dev@gmail.com>
2024-10-20 10:57:02 -0300

    Incluindo tratamento de erro

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 const rotaProdutos = require("./routes/produtos");
-const rotaPedidos = require('./routes/pedidos');
+const rotaPedidos = require("./routes/pedidos");
+const morgan = require("morgan");
 
-app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+app.use(morgan("dev"));
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
 
-// app.use('/teste', (req, res, next) => {
-//   res.status(200).send({
-//     message: "Ok, tudo certo!",
-//   });
-// });
+//QUANDO NÃO ENCONTRA ROTA ENTRA AQUI
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      menssagem: error.message,
+    },
+  });
+});
 
 module.exports = app;
